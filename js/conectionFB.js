8df8ee1676f818436593d9d38bc34f90006396f0
Manuel AMD <manuel_md123@hotmail.com>
2019-11-12 17:07:02 -0700

    Front de blockly

---
+++ js/conectionFB.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCFKfcL2YDnftkaygsN9-bljUzPtdDDXu4",
+  authDomain: "bpyia-fc64a.firebaseapp.com",
+  databaseURL: "https://bpyia-fc64a.firebaseio.com",
+  projectId: "bpyia-fc64a",
+  storageBucket: "bpyia-fc64a.appspot.com",
+  messagingSenderId: "45088818013",
+  appId: "1:45088818013:web:db0529eaa4cb32f7959eb4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
